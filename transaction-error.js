21a4b9ce1e0fc58ada8ffcc0d9938ec9969f132a
downtempo <17656228+marcus-timtow@users.noreply.github.com>
2018-07-02 14:43:39 +0200

    added AMD module loader compatibility

--- transaction-error.js
+++ transaction-error.js
@@ -1,61 +1,78 @@
-
-/**
- * A transaction error that may be encountered while handling an HTTP transaction.
- * 
- * @constructor
- * @extends Error
- * 
- * @argument {Error} [err]
- * @argument {string} msg
- * @argument {object|number} [details]
- * @argument {number} [details.status] HTTP response status
- */
-let TransactionError = function (err, msg, details) {
-    //Error.call(this, msg);
-    if (typeof err === "string") {
-        details = msg;
-        msg = err;
-        err = null;
+(function (deps, definition) {
+    if (!definition) {
+        definition = deps;
+        deps = [];
     }
-    this.message = msg;
-
-    if (typeof details === "number") {
-        details = {status: details};
+    if (!Array.isArray(deps)) {
+        deps = [deps];
     }
-    details = details || {};
-    if (details.status) {
-        this.status = details.status;
-    } else if (err && err.status) {
-        this.status = err.status;
+    if (typeof define === "function" && typeof define.amd === "object") {
+        define(deps, definition);
+    } else if (typeof module !== "undefined") {
+        module.exports = definition.apply(this, deps.map(function (dep) {
+            return require(dep);
+        }));
     } else {
-        this.status = 500;
+        throw new Error("missing module loader");
     }
-
-    if (err) {
-        if (err.tstack) {
-            this.tstack = err.tstack;
-            delete err.tstack;
-        } else {
-            this.tstack = [];
+})(function () {
+    /**
+     * A transaction error that may be encountered while handling an HTTP transaction.
+     * 
+     * @constructor
+     * @extends Error
+     * 
+     * @argument {Error} [err]
+     * @argument {string} msg
+     * @argument {object|number} [details]
+     * @argument {number} [details.status] HTTP response status
+     */
+    let TransactionError = function (err, msg, details) {
+        //Error.call(this, msg);
+        if (typeof err === "string") {
+            details = msg;
+            msg = err;
+            err = null;
         }
-        this.tstack.unshift(err);
-    }
-};
-//TransactionError.prototype = Object.create(Error.prototype);
-TransactionError.prototype.toJSON = function () {
-    return {
-        message: this.message,
-        status: this.status,
-        stack: this.tstack && this.tstack.map(function (err) {
-            return (err.toJSON && err.toJSON()) || {message: err.message};
-        })
+        this.message = msg;
+
+        if (typeof details === "number") {
+            details = {status: details};
+        }
+        details = details || {};
+        if (details.status) {
+            this.status = details.status;
+        } else if (err && err.status) {
+            this.status = err.status;
+        } else {
+            this.status = 500;
+        }
+
+        if (err) {
+            if (err.tstack) {
+                this.tstack = err.tstack;
+                delete err.tstack;
+            } else {
+                this.tstack = [];
+            }
+            this.tstack.unshift(err);
+        }
     };
-};
-TransactionError.prototype.toString = function () {
-    return this.status + " - " + this.message + (this.tstack ? ("\n\t" + this.tstack.map(function (err) {
-        return err.toString();
-    }).join("\n\t")) : "");
-};
+//TransactionError.prototype = Object.create(Error.prototype);
+    TransactionError.prototype.toJSON = function () {
+        return {
+            message: this.message,
+            status: this.status,
+            stack: this.tstack && this.tstack.map(function (err) {
+                return (err.toJSON && err.toJSON()) || {message: err.message};
+            })
+        };
+    };
+    TransactionError.prototype.toString = function () {
+        return this.status + " - " + this.message + (this.tstack ? ("\n\t" + this.tstack.map(function (err) {
+            return err.toString();
+        }).join("\n\t")) : "");
+    };
 
-module.exports = TransactionError;
-
+    return TransactionError;
+});
